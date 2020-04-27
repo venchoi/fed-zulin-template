94beeb28ca96f6fffaea72ec71a9f73bdebd66f4
venchoi <chenwh03@mingyuanyun.com>
2020-04-27 14:02:34 +0800

    feat(layout): :fire: fix some override style

--- src/components/FedFooter/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './index.less';
-
-const Footer = () => (
-    <div className="footer">Copyright © {new Date().getFullYear()} 明源云空间 版权所有 鄂ICP备15101856号-1</div>
-);
-
-export default Footer;
--- src/routers/layout/index.tsx
+++ src/routers/layout/index.tsx
@@ -2,9 +2,9 @@ import React, { ReactElement } from 'react';
 import { message } from 'antd';
 //@ts-ignore
 import * as queryString from 'query-string';
-import Footer from '../../components/FedFooter';
-import Header from '../../components/FedHeader';
-import Menu from '../../components/FedMenu';
+import { Layout as AntLayout } from 'antd';
+import FedHeader from '../../components/FedHeader';
+import FedMenu from '../../components/FedMenu';
 import { getHomeBaseInfo, mockLogin } from '../../api';
 import { getItem } from '../../helper/getItem';
 import config from '../../config';
@@ -12,6 +12,8 @@ import { handleBaseInfo } from '../../helper/handleBaseInfo';
 import { AppInfo, User } from '../../components/FedHeader/interface';
 
 import './index.less';
+
+const { Header, Sider, Content, Footer } = AntLayout;
 
 type MenuMode = 'inline' | 'vertical';
 const { DEV } = config;
@@ -57,7 +59,7 @@ class Layout extends React.Component<Props, State> {
     }
 
     componentDidMount() {
-        this.getBaseInfo()
+        this.getBaseInfo();
     }
 
     public render() {
@@ -74,22 +76,30 @@ class Layout extends React.Component<Props, State> {
         } = this.state;
         const nav = getItem(appList, appCode, 'key');
         return (
-            <div className="viewport-container">
-                <div className={menuMode === 'inline' ? 'viewport-menu-inline-space' : 'viewport-menu-vertical-space'}>
-                    <Menu
+            <AntLayout>
+                <Sider>
+                    <FedMenu
                         logoUrl={logoIcon}
                         menuList={(nav && nav.children) || []}
                         workflow={workflow}
                         menuMode={menuMode}
                         onMenuModeChange={this.setMenuMode}
                     />
-                </div>
-                <div className="viewport-right-space">
-                    <Header appList={appList} user={user} logoutUrl={logoutUrl} personalCenterUrl={personalCenterUrl} />
-                    <div className="content-space">{children}</div>
-                    <Footer />
-                </div>
-            </div>
+                </Sider>
+                <AntLayout>
+                    <Header>
+                        <FedHeader
+                            appList={appList}
+                            appCode={appCode}
+                            user={user}
+                            logoutUrl={logoutUrl}
+                            personalCenterUrl={personalCenterUrl}
+                        />
+                    </Header>
+                    <Content>{children}</Content>
+                    <Footer>Copyright © {new Date().getFullYear()} 明源云空间 版权所有 鄂ICP备15101856号-1</Footer>
+                </AntLayout>
+            </AntLayout>
         );
     }
 
@@ -104,14 +114,14 @@ class Layout extends React.Component<Props, State> {
         const res = await getHomeBaseInfo({}, 'GET');
         if (res.result) {
             const props: any = handleBaseInfo(res.data);
-            this.setState({ ...props })
+            this.setState({ ...props });
         } else {
             message.error(res.msg);
         }
     };
 
     setMenuMode = (val: MenuMode) => {
-        this.setState({ menuMode: val })
+        this.setState({ menuMode: val });
     };
 }
 export default Layout;
