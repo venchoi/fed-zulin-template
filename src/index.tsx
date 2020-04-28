322c714deee699bfa584404d599f8734279cb91d
venchoi <chenwh03@mingyuanyun.com>
2020-04-28 17:37:17 +0800

    chore(i18n): antd local provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import '@babel/polyfill';
 import React from 'react';
-import { BrowserRouter } from 'dva/router';
-import App from './app';
 import dva from 'dva';
+import { BrowserRouter } from 'dva/router';
+import { ConfigProvider } from 'antd';
+import zhCN from 'antd/lib/locale-provider/zh_CN';
+import App from './app';
 import model1 from './model/count';
 import model2 from './model/list';
 import './assets/init/normalize.css';
@@ -13,8 +15,10 @@ const app = dva({
 app.model(model1);
 app.model(model2);
 app.router((obj: any) => (
-    <BrowserRouter>
-        <App history={obj.history} getState={obj.app._store.getState} dispatch={obj.app._store.dispatch} />
-    </BrowserRouter>
+    <ConfigProvider locale={zhCN}>
+        <BrowserRouter>
+            <App history={obj.history} getState={obj.app._store.getState} dispatch={obj.app._store.dispatch} />
+        </BrowserRouter>
+    </ConfigProvider>
 ));
 app.start('#root');
