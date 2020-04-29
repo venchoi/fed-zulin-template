935b55006fb565a7a4e7bb1110944758a2a74260
谭金杰 <tanjj02@mingyuanyun.com>
2020-04-29 14:59:40 +0800

    update memory

--- src/MemoryShare/index.ts
+++ src/MemoryShare/index.ts
@@ -5,8 +5,8 @@ import cookie from './cookie';
 import localStore from './localStorage';
 import sessionStore from './sessionStorage';
 
-// export const vm = new WeakMap(); //键为对象
-// export const m = new Map(); //键为任意
+export const vm = new WeakMap(); //键为对象
+export const m = new Map(); //键为任意
 export const Cookies = cookie;
 export const Local = localStore;
 export const Session = sessionStore;
