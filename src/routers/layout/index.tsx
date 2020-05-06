89b990712eecf84941ea1aaabef16019ef6f70a6
venchoi <chenwh03@mingyuanyun.com>
2020-05-06 18:03:33 +0800

    style(layout)

--- src/routers/layout/index.tsx
+++ src/routers/layout/index.tsx
@@ -71,14 +71,16 @@ class Layout extends React.Component<Props, State> {
         return (
             <AntLayout style={{ minHeight: '100vh' }} className="main">
                 <Sider
-                    style={{ minHeight: '100vh' }}
+                    style={{ minHeight: '100vh', maxHeight: '100vh' }}
                     trigger={<CollapseItem collapsed={collapsed} />}
                     collapsible
                     collapsed={collapsed}
                     onCollapse={this.onCollapse}
                 >
                     <Logo collapsed={collapsed} logoUrl={logoIcon} />
-                    <FedMenu collapsed={collapsed} menuList={(nav && nav.children) || []} workflow={workflow} />
+                    <div style={{ maxHeight: 'calc(100vh - 56px)', overflowY: 'scroll' }}>
+                        <FedMenu collapsed={collapsed} menuList={(nav && nav.children) || []} workflow={workflow} />
+                    </div>
                 </Sider>
                 <AntLayout>
                     <Header>
