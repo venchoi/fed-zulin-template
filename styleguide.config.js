551c0f64d14924a0aae43856193247ea1dcde1e9
venchoi <chenwh03@mingyuanyun.com>
2020-05-08 21:16:27 +0800

    doc(section)

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,6 +1,13 @@
 module.exports = {
     webpackConfig: require('./config/webpack.dev.config.js'),
-    components: 'src/components/**/*.{js,jsx,ts,tsx}',
+    pagePerSection: true,
+    sections: [{
+      name: 'UI Components',
+      components: 'src/components/**/*.{js,jsx,ts,tsx}',
+    }, {
+      name: 'helpers',
+      components: 'src/helper/**/*.{js,jsx,ts,tsx}',
+    }],
     showUsage: true,
     showCode: true,
     title: '云空间通用组件'
