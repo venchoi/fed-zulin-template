41e3c4576b72db18885afb7a56c505a6bc0e2a2b
venchoi <chenwh03@mingyuanyun.com>
2020-05-11 00:41:58 +0800

    component(FedPagination): :art: fix style & default props

--- src/components/FedPagination/index.tsx
+++ src/components/FedPagination/index.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
-import { Pagination } from 'antd';
+import { Pagination, Divider } from 'antd';
 import { PaginationProps } from 'antd/es/pagination';
 import './index.less';
 
-const FedPagination = ({ ...props }: PaginationProps) => {
+const FedPagination = ({
+    hideOnSinglePage = true,
+    showSizeChanger = true,
+    pageSizeOptions = ['10', '20', '30', '50'],
+    defaultCurrent = 1,
+    ...props
+}: PaginationProps) => {
     return (
-        <div className="fed-pagination">
-            <Pagination {...props} />
-            {/* slot */}
+        <div className="fed-pagination-container">
+            <Divider />
+            <div className="fed-pagination">
+                <Pagination
+                    hideOnSinglePage={hideOnSinglePage}
+                    showSizeChanger={showSizeChanger}
+                    pageSizeOptions={pageSizeOptions}
+                    defaultCurrent={defaultCurrent}
+                    {...props}
+                />
+                {/* TODO slot */}
+            </div>
         </div>
     );
 };
