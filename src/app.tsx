deb98b79750414dab2591f014d4933f60de830ab
venchoi <chenwh03@mingyuanyun.com>
2020-05-12 22:29:51 +0800

    feat(suspend): lazy loading component

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Route, Switch, Router, BrowserRouter, Redirect } from 'dva/router';
-// import dynamic from 'dva/dynamic'
-import UI from './routers/ui';
+import { Spin } from 'antd';
 import Layout from './routers/layout';
 import ReportList from './routers/report';
-import NoRights from './routers/interceptors/noRights';
 import NotFoundPage from './routers/interceptors/notFoundPage';
 import { History } from 'history';
 interface Props {
@@ -12,45 +10,49 @@ interface Props {
     getState?: any;
     dispatch?: any;
 }
+
+const routes = [
+    {
+        path: '/ui',
+        // @ts-ignore
+        component: lazy(() => import('./routers/ui')),
+    },
+    {
+        path: '/report',
+        // @ts-ignore
+        component: lazy(() => import('./routers/report')),
+    },
+    {
+        path: '/noright',
+        // @ts-ignore
+        component: lazy(() => import('./routers/interceptors/noRights')),
+    },
+    {
+        path: '/404',
+        // @ts-ignore
+        component: lazy(() => import('./routers/interceptors/notFoundPage')),
+    },
+];
+
 export default class App extends React.PureComponent<Props> {
     public render() {
         return (
             <Layout>
                 <BrowserRouter basename="middleground">
-                    <Switch>
-                        <Route
-                            path="/ui"
-                            component={() => {
-                                return <UI />;
-                            }}
-                        />
-                        <Route
-                            path="/report"
-                            component={() => {
-                                return <ReportList history={this.props.history} />;
-                            }}
-                        />
-                        <Route
-                            path="/report/:type"
-                            component={() => {
-                                return <ReportList history={this.props.history} />;
-                            }}
-                        />
-                        <Route
-                            path="/noright"
-                            component={() => {
-                                return <NoRights history={this.props.history} />;
-                            }}
-                        />
-                        <Route path="/404" component={NotFoundPage} />
-                        <Route
-                            path="/"
-                            component={() => {
-                                return <ReportList history={this.props.history} />;
-                            }}
-                        />
-                        <Redirect exact from="/*" to="/report?_smp=Rental.Reort" />
-                    </Switch>
+                    <Suspense fallback={<Spin size="large" tip="Loading..."></Spin>}>
+                        <Switch>
+                            {routes.map(item => {
+                                return <Route path={item.path} component={item.component} />;
+                            })}
+                            <Route
+                                path="/"
+                                component={() => {
+                                    return <ReportList history={this.props.history} />;
+                                }}
+                            />
+                            <Redirect exact from="/*" to="/report?_smp=Rental.Report" />
+                        </Switch>
+                    </Suspense>
                 </BrowserRouter>
             </Layout>
         );
