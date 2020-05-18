608ef336e1fa551da6a99c7fdafe648e89112d54
谭金杰 <tanjj02@mingyuanyun.com>
2020-05-18 15:09:13 +0800

    update router config

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,7 @@
-import React, { lazy, Suspense } from 'react';
-import { Route, Switch, Router, BrowserRouter, Redirect } from 'dva/router';
+import React from 'react';
+import { Route, Switch, BrowserRouter, Redirect } from 'dva/router';
 import { Spin } from 'antd';
 import Layout from './routers/layout';
-import ReportList from './routers/report';
-import NotFoundPage from './routers/interceptors/notFoundPage';
-import Report from './routers/report';
-import UI from './routers/ui';
-import NoRights from './routers/interceptors/noRights';
-import Export from './routers/export';
-
-import { History } from 'history';
 import { RouteComponentProps } from 'dva/router';
 import Loadable from 'react-loadable';
 interface Props extends RouteComponentProps {
@@ -62,22 +54,20 @@ export default class App extends React.PureComponent<Props> {
     public render() {
         return (
             <Layout>
-                <BrowserRouter basename="middleground">
-                    <Router history={this.props.history}>
-                        {/* <Suspense fallback={<Spin size="large" tip="Loading..."></Spin>}> */}
-                        <Switch>
-                            {routes.map(item => {
-                                return <Route path={item.path} component={item.component} key={item.path} />;
-                            })}
-                            <Route
-                                path="/"
-                                component={() => {
-                                    return <ReportList history={this.props.history} />;
-                                }}
-                            />
-                            <Redirect exact from="/*" to="/report?_smp=Rental.Report" />
-                        </Switch>
-                    </Router>
+                <BrowserRouter basename="/middleground">
+                    {/* <Suspense fallback={<Spin size="large" tip="Loading..."></Spin>}> */}
+                    <Switch>
+                        {routes.map(item => {
+                            return <Route path={item.path} component={item.component} key={item.path} />;
+                        })}
+                        {/* <Route
+                            path="/"
+                            component={() => {
+                                return <ReportList history={this.props.history} />;
+                            }}
+                        /> */}
+                        <Redirect exact from="/*" to="/report?_smp=Rental.Report" />
+                    </Switch>
                     {/* </Suspense> */}
                 </BrowserRouter>
             </Layout>
