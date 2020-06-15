d4e4a561987825f52c450138a20e10cb795addd4
venchoi <chenwh03@mysoft.com.cn>
2020-06-15 18:28:14 +0800

    feat(meter): adjustment

--- src/routers/meter/components/adjustmentTimeLine.tsx
+++ src/routers/meter/components/adjustmentTimeLine.tsx
@@ -10,8 +10,12 @@ interface IProps {
     id: string;
 }
 interface IState {
-    data: IStandardPriceRecord[];
+    data: ITimeItem[];
     rangeDate: Moment[];
+}
+interface ITimeItem {
+    price: number;
+    date: ''
 }
 class AdjustmentChart extends React.Component<IProps, IState> {
     constructor(props: IProps) {
@@ -24,36 +28,53 @@ class AdjustmentChart extends React.Component<IProps, IState> {
     public componentDidMount() {
         this.fetchData();
     }
+    // 从startDate到endDate, 每隔一个月填充一个点
+    public fillTimeItem = (startDate: Moment, endDate: Moment, price: number) => {
+        const duration = moment.duration(endDate.diff(startDate));
+        const durationYears = duration.years();
+        const durationMonths = duration.months();
+        const fillList = Array(durationYears * 12 + durationMonths);
+        for (let i = 0; i < fillList.length; i++) {
+            console.log('i startDate', startDate)
+            fillList[i] = {
+                price: price,
+                date: startDate.add(1, 'M').format('YYYY-MM-DD'),
+            };
+        }
+        return fillList
+    }
     public fetchData = async () => {
         const { id } = this.props;
         const { rangeDate } = this.state;
         const start_date = rangeDate[0].format('YYYY-MM-DD');
         const end_date = rangeDate[1].format('YYYY-MM-DD');
-        let { data: result = [] } = await getStandardPriceRecord({ meter_standard_price_id: id, start_date, end_date });
-        result.map((item: IStandardPriceRecord) => {
+        let timeLine: ITimeItem[] = []
+        let { data = [] } = await getStandardPriceRecord({ meter_standard_price_id: id, start_date, end_date });
+        data.map((item: IStandardPriceRecord) => {
             item.price = +item.price;
+            item.step_data = JSON.parse(item.step_data || '[]')
+            if (item.is_step === '1') {
+                // 阶梯价需要填充数据
+            } else {
+
+            }
+            if (item.end_date) {
+                const fillList = this.fillTimeItem(moment(item.start_date, 'YYYY-MM-DD'), moment(item.end_date, 'YYYY-MM-DD'), +item.price);
+                console.log('item', item.start_date, item.end_date, fillList)
+                timeLine = timeLine.concat(fillList);
+            }
             return item;
         });
-        const lastItem = (result.length && result[result.length - 1]) || {};
+        const lastItem = (data.length && data[data.length - 1]) || {};
         // 最后一个的结束日期未空，说明未来的时间里都是用的这个单价，剩下的数据需要自动填充
         if (!lastItem.end_date) {
             const lastDate = moment(lastItem.start_date.split('-'));
-            const duration = moment.duration(rangeDate[1].diff(lastDate));
-            const durationYears = duration.years();
-            const durationMonths = duration.months();
-            const fillList = Array(durationYears * 12 + durationMonths).fill({
-                price: lastItem.price,
-            });
-            for (let i = 0; i < fillList.length; i++) {
-                fillList[i] = {
-                    price: lastItem.price,
-                    start_date: lastDate.add(1, 'M').format('YYYY-MM-DD'),
-                };
-            }
-            result = result.concat(fillList);
+            const fillList = this.fillTimeItem(lastDate, rangeDate[1], +lastItem.price);
+            console.log('timeLine', lastItem.start_date, lastDate.format('YYYY-MM-DD'), rangeDate[1].format('YYYY-MM-DD'))
+            timeLine = timeLine.concat(fillList);
         }
         this.setState({
-            data: result,
+            data: timeLine,
         });
     };
     public render() {
@@ -68,7 +89,7 @@ class AdjustmentChart extends React.Component<IProps, IState> {
             <>
                 <Chart height={300} data={data} scale={cols} autoFit>
                     <Legend />
-                    <Axis name="start_date" />
+                    <Axis name="date" />
                     <Axis name="price" />
                     <Tooltip
                         crosshairs={{
@@ -77,7 +98,7 @@ class AdjustmentChart extends React.Component<IProps, IState> {
                     />
                     <Geom
                         type="line"
-                        position="start_date*price"
+                        position="date*price"
                         size={2}
                         color={['price', ['#0D86FF']]}
                         shape={'hv'}
