efe4b2c3ead0d40552bb761f93776a2a8bc5f776
venchoi <chenwh03@mysoft.com.cn>
2020-06-15 19:50:27 +0800

    feat(meter): timeline

--- src/routers/meter/components/adjustmentTimeLine.tsx
+++ src/routers/meter/components/adjustmentTimeLine.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 // import { Slider } from 'antd'
-import { Chart, Geom, Axis, Tooltip, Legend, Slider } from 'bizcharts';
+import { Chart, Geom, Axis, Tooltip, Legend, Interaction } from 'bizcharts';
 import { cloneDeep } from 'lodash';
 import moment, { Moment } from 'moment';
 import { IStandardPriceRecord } from '@t/meter';
@@ -15,7 +15,7 @@ interface IState {
 }
 interface ITimeItem {
     price: number;
-    date: ''
+    date: string
 }
 class AdjustmentChart extends React.Component<IProps, IState> {
     constructor(props: IProps) {
@@ -28,50 +28,48 @@ class AdjustmentChart extends React.Component<IProps, IState> {
     public componentDidMount() {
         this.fetchData();
     }
-    // 从startDate到endDate, 每隔一个月填充一个点
-    public fillTimeItem = (startDate: Moment, endDate: Moment, price: number) => {
-        const duration = moment.duration(endDate.diff(startDate));
-        const durationYears = duration.years();
-        const durationMonths = duration.months();
-        const fillList = Array(durationYears * 12 + durationMonths);
-        for (let i = 0; i < fillList.length; i++) {
-            fillList[i] = {
-                price: price,
-                date: startDate.add(1, 'M').format('YYYY-MM-DD'),
-            };
-        }
-        return fillList
+    public handleTimeLine = (timeLine: IStandardPriceRecord[]): ITimeItem[] => {
+        const flatTimeLine: ITimeItem[] = []; //  填充后的点的数据
+        const { rangeDate } = this.state
+        timeLine.map(item => {
+            // item区间数据，可能为点
+            const start = item.start_date // 第一个区间的起始时间
+            const end = item.end_date // 第一个区间的结束时间
+            const momentStart = moment(start, 'YYYY-MM-DD')
+            if (start === end) {
+                // 起始时间 === 结束时间时，为一个点，直接推
+                flatTimeLine.push({
+                    date: start,
+                    price: +item.price
+                })
+            } else if (!end) {
+                // 最后一个点，未来的区间
+                while(momentStart.isBefore(rangeDate[1], 'day')) {
+                    flatTimeLine.push({
+                        date: momentStart.format('YYYY-MM-DD'),
+                        price: +item.price
+                    })
+                    momentStart.add(1, 'd');
+                }
+            } else {
+                while(momentStart.isBefore(end, 'day')) {
+                    flatTimeLine.push({
+                        date: momentStart.format('YYYY-MM-DD'),
+                        price: +item.price
+                    })
+                    momentStart.add(1, 'd');
+                }
+            }
+        })
+        return flatTimeLine;
     }
     public fetchData = async () => {
         const { id } = this.props;
         const { rangeDate } = this.state;
         const start_date = rangeDate[0].format('YYYY-MM-DD');
         const end_date = rangeDate[1].format('YYYY-MM-DD');
-        let timeLine: ITimeItem[] = []
         let { data = [] } = await getStandardPriceRecord({ meter_standard_price_id: id, start_date, end_date });
-        data.map((item: IStandardPriceRecord) => {
-            item.price = +item.price;
-            item.step_data = JSON.parse(item.step_data || '[]')
-            if (item.is_step === '1') {
-                // 阶梯价需要填充数据
-            } else {
-
-            }
-            if (item.end_date) {
-                const fillList = this.fillTimeItem(moment(item.start_date, 'YYYY-MM-DD'), moment(item.end_date, 'YYYY-MM-DD'), +item.price);
-                console.log('item', item.start_date, item.end_date, fillList)
-                timeLine = timeLine.concat(fillList);
-            }
-            return item;
-        });
-        const lastItem = (data.length && data[data.length - 1]) || {};
-        // 最后一个的结束日期未空，说明未来的时间里都是用的这个单价，剩下的数据需要自动填充
-        if (!lastItem.end_date) {
-            const lastDate = moment(lastItem.start_date.split('-'));
-            const fillList = this.fillTimeItem(lastDate, rangeDate[1], +lastItem.price);
-            console.log('timeLine', lastItem.start_date, lastDate.format('YYYY-MM-DD'), rangeDate[1].format('YYYY-MM-DD'))
-            timeLine = timeLine.concat(fillList);
-        }
+        let timeLine: ITimeItem[] = this.handleTimeLine(data)
         this.setState({
             data: timeLine,
         });
@@ -102,6 +100,7 @@ class AdjustmentChart extends React.Component<IProps, IState> {
                         color={['price', ['#0D86FF']]}
                         shape={'hv'}
                     />
+                    <Interaction type="legend-filter"/>
                 </Chart>
             </>
         );
