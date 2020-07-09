e59bcf69187862b1df39eb98775c03ec3fd3932b
谭金杰 <tanjj02@mingyuanyun.com>
2020-07-09 12:17:11 +0800

    feat: 测试组件库

--- src/routers/report/index.tsx
+++ src/routers/report/index.tsx
@@ -88,42 +88,6 @@ const ReportList = () => {
             },
         },
     ];
-    const dataSource = [
-        {
-            key: '1',
-            name: '胡彦斌',
-            age: 32,
-            address: '西湖区湖底公园1号',
-        },
-        {
-            key: '2',
-            name: '胡彦祖',
-            age: 42,
-            address: '西湖区湖底公园1号',
-        },
-    ];
-
-    const column = [
-        {
-            title: '姓名',
-            dataIndex: 'name',
-            key: 'name',
-            width: 100,
-        },
-        {
-            title: '年龄',
-            dataIndex: 'age',
-            key: 'age',
-            width: 100,
-        },
-        {
-            title: '住址',
-            dataIndex: 'address',
-            key: 'address',
-            width: 100,
-        },
-    ];
-
     return (
         <div className="report layout-list">
             <Card
