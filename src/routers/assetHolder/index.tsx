49e6baa06361a06fd0539b738fb93aac308ebe61
yangb03 <yangb03@mingyuanyun.com>
2020-07-24 18:20:16 +0800

    fix: 修复bug.

--- src/routers/assetHolder/index.tsx
+++ src/routers/assetHolder/index.tsx
@@ -16,7 +16,7 @@ import {
 } from '@/services/assetHolder';
 import TreeProjectSelect from '@c/TreeProjectSelect';
 import { IGetCustomLayout, IField } from '@t/assetHolder';
-import { IHeader } from '../../constants/layoutConfig';
+import { IHeader, asset_holder_list_layout } from '../../constants/layoutConfig';
 import { customType, cooperateStatus } from '../../constants/index';
 import calcBodyHeight from './utils';
 import FedPagination from '@c/FedPagination';
